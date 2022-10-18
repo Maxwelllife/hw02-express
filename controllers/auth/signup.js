3befb7436e48a2f2681f4d3a2d729037d5caf601
Maksym Pokulytyi <mlightpro@gmail.com>
2022-10-18 16:53:21 +0300

    add task 6

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,7 +1,8 @@
 // const { Conflict } = require("http-errors"); можна использовать если нет такой функции как RequestError
 const { User } = require("../../models");
-const { RequestError } = require("../../helpers");
+const { RequestError, sendEmail } = require("../../helpers");
 const bcrypt = require("bcryptjs");
+const { v4: uuidv4 } = require("uuid");
 
 const signup = async (req, res) => {
   const { name, email, subscription, password } = req.body;
@@ -9,14 +10,24 @@ const signup = async (req, res) => {
   if (user) {
     throw RequestError(409, "Email in use");
   }
+  const verificationToken = uuidv4();
   const hashPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
   // если его еще нет то сохраняем в базе
   const result = await User.create({
     name,
     email,
     subscription,
+    verificationToken,
     password: hashPassword,
   });
+  // создаем письмо
+  const mail = {
+    to: email,
+    subject: "Подтверждение email",
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Подтвердить email</a>`,
+  };
+  // отправляем письмо
+  await sendEmail(mail);
   // res.status(201).json(
   //   {
   //   email: result.email,
@@ -30,6 +41,7 @@ const signup = async (req, res) => {
       email: result.email,
       name: result.name,
       subscription: result.subscription,
+      verificationToken: result.verificationToken,
     },
   });
 };
